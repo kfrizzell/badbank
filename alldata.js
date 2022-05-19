8c954856ffeaeb755ebd34685d8f04e3ace08da0
Katrina Frizzell <93966332+kfrizzell@users.noreply.github.com>
2022-05-19 09:16:46 -0500

    Update alldata.js

--- alldata.js
+++ alldata.js
@@ -1,10 +1,45 @@
 //all data available from user input
 function AllData(){
   const ctx = React.useContext(UserContext);
+  
   return (
-    <>
-    <h5>All User Data</h5>
-    {JSON.stringify(ctx)}<br/>
-    </>
-  );
+    <div className="card-group">
+    <div className="card">
+      <div className="card-header bg-info text-center text-blue">
+        Account Holder
+      </div>
+      {ctx.users.map(({name}) => { return ( <>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item text-center">{name}</li>
+      </ul> </>)})} 
+    </div> 
+    <div className="card">
+        <div className="card-header bg-info text-center text-blue">
+          Email Address
+        </div>
+        {ctx.users.map(({email}) => { return ( <>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item text-center">{email}</li> 
+        </ul> </>)})}
+      </div>
+      <div className="card">
+        <div className="card-header bg-info text-center text-blue">
+          Password
+        </div>
+        {ctx.users.map(({balance}) => { return ( <>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item text-center">$ {balance}</li>
+        </ul> </>)})}
+      </div>
+        {ctx.users.map(({password}) => { return ( <>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item text-center">{password}</li>
+        </ul> </>)})}
+      </div>
+      <div className="card">
+        <div className="card-header bg-info text-center text-blue">
+          Account Balance
+      </div>       
+    </div> 
+);    
 }
