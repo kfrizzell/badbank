2c991a00bd7a18b8cd4f93edca301d5aaabeaaa7
Katrina Frizzell <93966332+kfrizzell@users.noreply.github.com>
2022-05-19 07:24:05 -0500

    Update balance.js

--- balance.js
+++ balance.js
@@ -12,7 +12,7 @@ function Balance(){
         <>
           <ul className="list-group list-group-flush make-center bg-dark">
             <li className="list-group-item make-center">
-              Total Balance ${ctx.users[0].balance}
+              Total Balance ${ctx.users[0].balance};
               Name ${ctx.users[0].name}
             </li>
           </ul>
