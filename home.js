7fe8563f1d9ed62ecb033253bd1f9538033ae830
Katrina Frizzell <93966332+kfrizzell@users.noreply.github.com>
2022-08-04 03:43:05 -0500

    Update home.js

--- home.js
+++ home.js
@@ -4,7 +4,7 @@ function Home(){
     <Card
       txtcolor="blue"
       header="BadBank Home"
-      title="Welcome to BadBank"
+      title="Welcome to Katrina's Full Stack BadBank"
       text="Where expectations are as low as your account balance."
       body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
     />
