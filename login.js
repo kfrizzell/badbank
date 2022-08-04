21133985e67a2bfa9aa51913700c27a21295cc7b
Katrina Frizzell <93966332+kfrizzell@users.noreply.github.com>
2022-08-04 03:29:08 -0500

    Update login.js

--- login.js
+++ login.js
@@ -45,7 +45,7 @@ function Login(){
               <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
               Password<br/>
               <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
-              <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+              <button type="submit" className="btn btn-light" onClick={handleCreate}>Login</button>
               </>
             ):(
               <>
